78702d9dea94341504f581ccf2384830b1a97aec
Shifu <aemre3113@gmail.com>
2024-05-08 14:41:08 +0300

    config dosyaları düzenlendi.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  //output: "export",
+  // reactStrictMode: true,
+  i18n: {
+    locales: ["en", "tr"],
+    defaultLocale: "tr",
+    localeDetection: false,
+  },
+  trailingSlash: true,
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "",
+        port: "",
+        pathname: "/images/**",
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
